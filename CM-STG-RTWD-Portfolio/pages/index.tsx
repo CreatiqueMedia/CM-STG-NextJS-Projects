1f1a65c627811917797c0bbcad6c4b487f2064c3
RonTheWebDev <ron@creatiquemedia.com>
2022-09-22 13:59:33 -0500

    uploaded files

---
+++ CM-STG-RTWD-Portfolio/pages/index.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-[rgb(8,9,56)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+      <Head>
+        <title>Ron The Web Dev - Portfolio</title>
+      </Head>
+
+      <Header />
+
+      {/* Hero */}
+
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
+    </div>
+  );
+};
+
+export default Home;
